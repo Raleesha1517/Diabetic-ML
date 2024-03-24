2e4933ef59f7348a4228cfdba911dfe418bdf0b4
Raleesha Niwanthaka <132451534+Raleesha1517@users.noreply.github.com>
2024-03-24 21:51:22 +0530

    Add files via upload

---
+++ Multiple disease prediction.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 18:52:22 2024
+
+@author: heart
+"""
+
+import os
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+
+                           ['Diabetes Prediction',
+                            'Heart Disease Prediction',
+                            'Parkinsons Prediction'],
+                           menu_icon='hospital-fill',
+                           icons=['activity', 'heart', 'person'],
+                           default_index=0)
+    
+if selected == 'Diabetes Prediction':
+
+    # page title
+    st.title('Diabetes Prediction using ML')
+
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        BMI = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
+    with col2:
+        Age = st.text_input('Age of the Person')
+
+
+    # code for Prediction
+    diab_diagnosis = ''
+
+    # creating a button for Prediction
+
+    if st.button('Diabetes Test Result'):
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+
+        user_input = [float(x) for x in user_input]
+
+        diab_prediction = diabetes_model.predict([user_input])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+
+    st.success(diab_diagnosis)
